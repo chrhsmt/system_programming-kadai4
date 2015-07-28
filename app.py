da052fadfce525668d7208b7b59bcfade974908b
chrhsmt <chr@chrhsmt.com>
2015-07-28 16:10:43 +0900

    Refactoring

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function
+# from __future__ import absolute_import, division, print_function
 from flask import Flask, request, jsonify
 import urllib
 import urllib2
 import json
 import os
+import sys
+import logging
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 BOT_NAME = "harahe"
+HOTPAPPER_API_PATH = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
 class Message(object):
     """Slackのメッセージクラス"""
@@ -25,16 +30,26 @@ class Message(object):
     trigger_word = ""  # OutgoingWebhooksに設定したトリガー
 
     def __init__(self, params):
-        # self.token = params["token"]
-        # self.team_id = params["team_id"]
-        # self.channel_id = params["channel_id"]
-        # self.channel_name = params["channel_name"]
-        # self.timestamp = params["timestamp"]
-        # self.user_id = params["user_id"]
-        # self.user_name = params["user_name"]
-        self.text = params["text"]
-        # self.team_domain = params["team_domain"]
-        # self.service_id = params["service_id"]
+        if (params.has_key("token")):
+            self.token = params["token"]
+        if (params.has_key("team_id")):
+            self.team_id = params["team_id"]
+        if (params.has_key("channel_id")):
+            self.channel_id = params["channel_id"]
+        if (params.has_key("channel_name")):
+            self.channel_name = params["channel_name"]
+        if (params.has_key("timestamp")):
+            self.timestamp = params["timestamp"]
+        if (params.has_key("user_id")):
+            self.user_id = params["user_id"]
+        if (params.has_key("user_name")):
+            self.user_name = params["user_name"]
+        if (params.has_key("text")):
+            self.text = params["text"]
+        if (params.has_key("team_domain")):
+            self.team_domain = params["team_domain"]
+        if (params.has_key("service_id")):
+            self.service_id = params["service_id"]
         if (params.has_key("trigger_word")):
             self.trigger_word = params["trigger_word"]
 
@@ -43,45 +58,36 @@ class Message(object):
         res += "@{0.token}[channel={0.channel_name}, user={0.user_name}, text={0.text}]".format(self)
         return res
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/', methods=["POST"])
 def index():
-    print (request.form)
-    app.logger.info("----------")
     app.logger.debug(request.form)
-    try:
-        if (request.method == "POST"):
-            msg = Message(request.form)
-            app.logger.debug(msg)
-        if msg.user_name == BOT_NAME:
-            return ""
-        if BOT_NAME in msg.text:
-            return say(recommend(msg.text.split()[-1]))
-        else:
-            return ""
-    except Exception, e:
-        print (e)
-        return e
+    msg = Message(request.form)
+    app.logger.debug(msg)
+    if msg.user_name == BOT_NAME:
+        # BOT自身の投稿に反応させてしまうと無限ループになることもあるので反応させない
+        return ""
+    if BOT_NAME in msg.text:
+        return say(recommend(msg.text.split()[-1]))
     else:
-        pass
-    finally:
-        pass
+        return ""
 
 def recommend(keyword):
-    print ("recommend start")
-    keyword = urllib.quote(keyword.encode("UTF-8"))
-    print ("keyword %s" % keyword)
-    request = urllib2.Request("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?keyword=%s&format=json&is_open_time=now&key=%s" % (keyword, os.environ['API_KEY']))
-    print (request.get_full_url())
+    app.logger.debug("recommend start: [keyword=%s]" % keyword)
+    keyword = urllib.quote(keyword.strip().encode("UTF-8"))
+    request = urllib2.Request("%s?keyword=%s&format=json&is_open_time=now&key=%s" % (HOTPAPPER_API_PATH, keyword, os.environ['API_KEY']))
     response = urllib2.urlopen(request)
     json_obj = json.loads(response.read())
+    app.logger.debug("-- response --")
+    app.logger.debug(json_obj)
+    app.logger.debug("-- /response --")
     if (len(json_obj["results"]["shop"]) > 0):
         return json_obj["results"]["shop"][0]["name"]
     else:
-        return "Oops... none"
+        return "Oops... not found."
 
 def say(text):
     """Slackの形式でJSONを返す"""
-    print ("say start")
+    app.logger.debug("say start")
     return jsonify({
         "text": text, # 投稿する内容
         "username": BOT_NAME, # bot名
